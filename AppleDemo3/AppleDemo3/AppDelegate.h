8535ae38a79ba2cc1c4f8449342ca8a0f932acc4
beyondsjxc <33192425+beyondsjxc@users.noreply.github.com>
2022-11-09 09:48:13 +0800

    Add files via upload

---
+++ AppleDemo3/AppleDemo3/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AppleDemo3
+//
+//  Created by wade on 2022/11/9.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AppleDemo3/AppleDemo3/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  AppleDemo3
+//
+//  Created by wade on 2022/11/9.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
